b5a8099c73400945588b06cfcc5aebdba9859d62
Corruptthreat <drawte786@gmail.com>
2020-04-21 01:13:08 +0530

    final push

--- app/src/main/java/com/example/quiz_app/MainActivity.java
+++ app/src/main/java/com/example/quiz_app/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Button falseBtn;
     private Button trueBtn;
     private ImageButton nextBtn;
+    private ImageButton prevBtn;
     private TextView textview1;
     private int counterNext = 0;
     private Question [] questions = new Question[]{
@@ -32,26 +33,47 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         falseBtn = findViewById(R.id.btn_false);
         trueBtn = findViewById(R.id.btn_true);
         nextBtn = findViewById(R.id.btn_next);
+        prevBtn = findViewById(R.id.btn_prev);
         textview1 = findViewById(R.id.textview1);
 
         falseBtn.setOnClickListener(this);//register our buttons to click events
         trueBtn.setOnClickListener(this);
         nextBtn.setOnClickListener(this);
+        prevBtn.setOnClickListener(this);
     }
     @Override
     public void onClick(View v){
        switch (v.getId()){
            case R.id.btn_false:
-               Toast.makeText(MainActivity.this,"False",Toast.LENGTH_SHORT).show();
+            checkAns(false);
                break;
            case R.id.btn_true:
-               Toast.makeText(MainActivity.this,"True",Toast.LENGTH_SHORT).show();
+               checkAns(true);
        break;
            case R.id.btn_next:
                 counterNext = (counterNext+1)%questions.length;
-               Log.d("COUNTER","counter incremented" + counterNext);
-               textview1.setText(questions[counterNext].getAnswersResId());
+                updateQuestion();
                break;
+           case R.id.btn_prev:
+               if(counterNext!=0) {
+                   counterNext = (counterNext-1)%questions.length;
+                   updateQuestion();
+               }
        }
+    }
+    private void updateQuestion(){
+        Log.d("COUNTER","counter incremented" + counterNext);
+        textview1.setText(questions[counterNext].getAnswersResId());
+    }
+    private void checkAns(Boolean checkedAns){
+           Boolean choosedOption = questions[counterNext].isAnswerTrue();
+           int toastId = 0;
+           if(checkedAns==choosedOption){
+               toastId = R.string.correct_answer;
+           }
+           else{
+               toastId = R.string.wrong_answer;
+           }
+           Toast.makeText(MainActivity.this,toastId , Toast.LENGTH_SHORT).show();
     }
 }
